981b056f2863cd6f9e34c3703a20c98840550cec
Lydia Abebaw <lydiaabebaw@gmail.com>
2026-05-11 14:24:00 -0700

    MNTHC-51: Messaging database connection module

--- database/messaging_db.php
+++ database/messaging_db.php
@@ -1,25 +1,24 @@
 <?php 
-// MNTHC-51: Connect Messaging Database 
-// Created by: Lydia Abebaw 
- 
-class MessagingDatabase { 
+// MNTHC-51: Messaging Database by Lydia 
+class MessagingDB { 
     private $db; 
- 
-    public function __construct() { 
-        $this- = new PDO('mysql:host=localhost;dbname=maraki_db', 'root', ''); 
-        $this-, PDO::ERRMODE_EXCEPTION); 
+    public function __construct($db) { 
+        $this- = $db; 
     } 
- 
-    public function sendMessage($senderId, $receiverId, $message) { 
-        $sql = "INSERT INTO messages (sender_id, receiver_id, message) VALUES (?, ?, ?)"; 
+    public function sendMessage($sender, $receiver, $message) { 
+        $sql = "INSERT INTO messages (sender_id, receiver_id, message, created_at) VALUES (?, ?, ?, NOW())"; 
         $stmt = $this-
-        return $stmt-, $receiverId, $message]); 
+        return $stmt-, $receiver, $message]); 
     } 
- 
     public function getMessages($user1, $user2) { 
         $sql = "SELECT * FROM messages WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?) ORDER BY created_at"; 
         $stmt = $this-
         $stmt-, $user2, $user2, $user1]); 
         return $stmt-
+    } 
+    public function markAsRead($receiver, $sender) { 
+        $sql = "UPDATE messages SET is_read = 1, read_at = NOW() WHERE sender_id = ? AND receiver_id = ? AND is_read = 0"; 
+        $stmt = $this-
+        return $stmt-, $receiver]); 
     } 
 } 
