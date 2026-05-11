5b496358e00a9f39026a15ede1a4bd1ff389c613
mahi-a-1 <mahialemu0521@gmail.com>
2026-05-11 16:51:19 +0300

    MNTHC-59 #done Implement notification service - completed

--- api/notifications.php
+++ api/notifications.php
@@ -1,6 +1,6 @@
 <?php
 // api/notifications.php
-// MNTHC-59: Implement notification service
+// MNTHC-59: Implement notification service - completed
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT");
