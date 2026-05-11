b17e9a5d92d63cc7d8aa308909128525d403cb33
Abenezer Andualem <abeniandy2@gmail.com>
2026-05-11 18:25:32 +0300

    MNTHC-41 #done Create service page - Abenezer Andualem

--- api/service_page.php
+++ api/service_page.php
@@ -1,6 +1,6 @@
 <?php
 // api/service_page.php
-// MNTHC-41: Create service page - completed
+// MNTHC-41: Create service page - completed - author: Abenezer Andualem
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
